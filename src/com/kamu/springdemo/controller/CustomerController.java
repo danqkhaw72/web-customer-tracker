256eea4497dc95e77f42e9201b29bb96cb875f14
Dang Kham <danqkhaw72@gmail.com>
2020-08-03 15:19:29 +0700

    Test spring mvc contronller

---
+++ src/com/kamu/springdemo/controller/CustomerController.java
@@ -0,0 +1,17 @@
+package com.kamu.springdemo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/customer")
+public class CustomerController {
+	
+	@RequestMapping("/list")
+	public String listCustomer(Model theModel) {
+		
+		return "list-customer";
+	}
+
+}
