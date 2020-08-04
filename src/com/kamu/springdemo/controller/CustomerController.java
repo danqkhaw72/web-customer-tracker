9834b4973d4f478ec1201cbb6e20e56c30d4f6f1
Dang Kham <danqkhaw72@gmail.com>
2020-08-04 15:28:12 +0700

    Convert @RequestMapping to @GetMapping

--- src/com/kamu/springdemo/controller/CustomerController.java
+++ src/com/kamu/springdemo/controller/CustomerController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.kamu.springdemo.dao.CustomerDAO;
@@ -18,7 +19,7 @@ public class CustomerController {
 	@Autowired
 	private CustomerDAO customerDAO;
 	
-	@RequestMapping("/list")
+	@GetMapping("/list")
 	public String listCustomer(Model theModel) {
 		
 		// get customers from the dao
