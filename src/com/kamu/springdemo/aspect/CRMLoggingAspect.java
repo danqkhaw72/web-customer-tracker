1cd10c1b9846b8188e0826fadea6261566ef8297
Dang Kham <danqkhaw72@gmail.com>
2020-08-13 15:26:50 +0700

    Create Aspect

---
+++ src/com/kamu/springdemo/aspect/CRMLoggingAspect.java
@@ -0,0 +1,21 @@
+package com.kamu.springdemo.aspect;
+
+import java.util.logging.Logger;
+
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+public class CRMLoggingAspect {
+	
+	// setup logger
+	private Logger myLogger = Logger.getLogger(getClass().getName());
+	
+	// setup pointcut declarations
+	
+	// add @Before advice
+	
+	// add @AfterReturning advice
+
+}
